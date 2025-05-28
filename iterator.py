441aa18aff12a619da3136a5b35c5a7f232f6adf
sanika waghamare <sanikawaghamare42@gmail.com>
2025-05-28 16:05:54 +0530

    Update iterator.py

--- iterator.py
+++ iterator.py
@@ -1,7 +1,40 @@
-
+1)program1
 import sys
 list1=list(range(1,100))
 print(list1)
 print(sys.getsizeof(list1))
 x=iter(list1)
 print(x)
+
+
+
+2)  program2
+import sys
+list1=list(range(1,100))
+print(list1)
+print(sys.getsizeof(list1))
+x=iter(list1)
+print(x)
+while True:
+    try:
+        print(next(x))
+    except:
+        break
+print(sys.getsizeof(x))
+
+
+3)program3
+import sys
+list1=["aishwarya","vaishnavi","sanika"]
+x=iter(list1)
+print(x)
+while True:
+    try:
+        print(next(x))
+
+    except:
+        break
+   # print(next(x))
+print(sys.getsizeof(x))
+
+
